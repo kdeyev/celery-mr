71345ba5eec50d77ab0d9410dfcd4ad738a1d35c
Kostya Deev <kostya.deev@bluware.com>
2020-03-29 01:39:28 -0500

    simplify code

--- client.py
+++ client.py
@@ -1,45 +1,58 @@
 import time
-from celery import Celery, chord, chain
+import celery
 import tasks
+from toolz.itertoolz import partition_all, concat
 
 app = tasks.app
 
 def create_work(elements_count, chunk_size):
     """ A fast task for initiating our map function """
-    return tasks.mapreduce.delay(elements_count, chunk_size).id
+    data = partition_all(chunk_size, range(elements_count))
+
+    # break up our data into chunks and create a dynamic list of workers
+    maps = (tasks.map.s(x) for x in data)
+    mapreducer = celery.chord(maps)(tasks.reduce.s())
+
+    mapper = mapreducer.parent
+    reducer = mapreducer
+    
+    # required for celery.result.GroupResult.restore
+    mapper.save()
+    
+    return (mapper.id, reducer.id)
 
 
-def get_work(chord_id):
+def get_work(mapper_id, reducer_id):
     """ A fast task for checking our map result """
 
-    if app.AsyncResult(chord_id).ready():
-        result_id = app.AsyncResult(chord_id).get()['chord_id']
-    else:
-        return {'status': 'pending', 'stage': 1}
-
-    if app.AsyncResult(result_id).ready():
+    reducer = app.AsyncResult(reducer_id)
+    mapper = celery.result.GroupResult.restore(mapper_id)
+    
+    completed = mapper.completed_count()
+    if reducer.ready():
         return {
             'status': 'success',
-            'results': app.AsyncResult(result_id).get()}
+            'completed': completed,
+            'results': reducer.get()}
     else:
-        return {'status': 'pending', 'stage': 2}
+        return {'status': 'pending', 'completed': completed}
 
-def wait_for_task(my_id):
-    print(f"Waiting for task {my_id}")
+def wait_for_task(mapper_id, reducer_id):
+    print(f"Waiting for task {reducer_id}")
     
     for i in range(100):
         time.sleep(1)
-        results = get_work(my_id)
-        print(f"Task {my_id} status: {results['status']}")
+        results = get_work(mapper_id, reducer_id)
+        print(f"Task {reducer_id} status: {results}")
         if results['status'] == 'success':
             return results['results']
         
     return None 
         
 if __name__ == '__main__':
-    my_id = create_work(elements_count=100000,chunk_size=100)
-    print(f"Task started {my_id}")
+    mapper_id, reducer_id = create_work(elements_count=100000,chunk_size=100)
+    print(f"Task started {reducer_id}")
     
-    results = wait_for_task(my_id)
+    results = wait_for_task(mapper_id, reducer_id)
     
     print(f"Task execution result {results}")
--- tasks.py
+++ tasks.py
@@ -2,7 +2,7 @@ from celery import Celery
 from celery import Celery, chord, chain
 from celery.utils.log import get_task_logger
 
-from toolz.itertoolz import partition_all, concat
+
 import random
 import os
 import threading
@@ -30,14 +30,3 @@ def map(data):
     for chunk in data:
         results.append({"chunk": chunk, "count": 1, "data": random.randrange(10000)})
     return results
-
-
-@app.task(acks_late=True, autoretry_for=(Exception,), retry_kwargs={'max_retries': 5})
-def mapreduce(elements_count, chunk_size):
-    """ A long running task which splits up the input data to many workers """
-    data = partition_all(chunk_size, range(elements_count))
-
-    # break up our data into chunks and create a dynamic list of workers
-    maps = (map.s(x) for x in data)
-    mapreducer = chord(maps)(reduce.s())
-    return {'chord_id': mapreducer.id}
